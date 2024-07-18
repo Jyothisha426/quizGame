666212bbce1851bb015e00462adc5197c3cd376a
Jyothisha426 <jyothishab445@gmail.com>
2024-07-18 16:05:30 +0000

    Quiz game

---
+++ src/components/Header/index.js
@@ -0,0 +1,23 @@
+import './index.css'
+
+const Header = () => {
+  ;<nav className="header-container">
+    <img
+      src="https://imagetolink.com/ib/0JJvpMap3E.png"
+      alt="login website logo"
+      className="website-logo"
+    />
+    <button type="button" className="logout-btn-large">
+      Logout
+    </button>
+    <button type="button" className="logout-btn-small">
+      <img
+        src="https://imagetolink.com/ib/vzNykpskhs.png"
+        className="log-out-img"
+        alt="logout"
+      />
+    </button>
+  </nav>
+}
+
+export default Header
